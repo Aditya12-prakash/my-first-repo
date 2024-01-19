b2c6adc1eb94921f77a26655b662a916232bb1dc
Aditya Prakash <157092278+Aditya12-prakash@users.noreply.github.com>
2024-01-19 18:47:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+ var a=7;
+ var b= 8;
+ var sum;
+ sum= a+b;
+ console.log(sum);
+ if(a<b)
+       {
+        console.log(b+ " is big");
+       }
+else if(b<a){
+    console.log(a+ " is big" );
+      }
+ else{
+    console.log("Both are equal");
+ }
